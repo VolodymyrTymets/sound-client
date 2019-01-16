b9e37342a9737af316c700df3d0479dfc2a4150e
volodymyrtymets <volodymyrtymets@gmail.com>
2019-01-16 19:06:09 +0200

    build

---
+++ server/public/build/precache-manifest.40696ab56ec68e496b5e6c0fe669c1a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "26a2e05eb449a9cf40b3",
+    "url": "/static/js/main.26a2e05e.chunk.js"
+  },
+  {
+    "revision": "7aa8210004373ee46197",
+    "url": "/static/js/1.7aa82100.chunk.js"
+  },
+  {
+    "revision": "26a2e05eb449a9cf40b3",
+    "url": "/static/css/main.8f8369e7.chunk.css"
+  },
+  {
+    "revision": "7aa8210004373ee46197",
+    "url": "/static/css/1.1465c695.chunk.css"
+  },
+  {
+    "revision": "74f1226fe028c37331bdffc6e0d6f279",
+    "url": "/index.html"
+  }
+];

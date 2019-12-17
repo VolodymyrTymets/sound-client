2d5714bfd8670d01e424c617f4c25c9cc9fe71a1
volodymyr <volodymyrtymets@gmail.com>
2019-12-17 18:40:39 +0200

    Revert "remove unpipe"
    This reverts commit 4781e028

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -4,14 +4,21 @@ const { onMicRate } = require('./mic-level');
 const { onRLNNotify } = require('./RLN-notigy');
 const { socketClients } = require('../utils/SocketClients');
 
+let lastStream = null;
+
 const initControllers = (config, io) => {
     io.on('connection', client => {
         console.log('[Socket] --> client is connected.', client.id);
         socketClients.setClient(client);
+        const micInputStream = global.mic.getMicInputStream();
         // send mic stream to client
         const stream = ss.createStream();
-        const micInputStream = global.mic.getMicInputStream();
+        if(lastStream) {
+          micInputStream && micInputStream.unpipe(lastStream);
+        }
+        lastStream = stream;
         micInputStream && micInputStream.pipe(stream);
+
         ss(client).emit('mic-stream', stream, { mic: {
                 rate: config.mic.rate,
                 channels: config.mic.channels,
--- server/src/utils/SocketClients.js
+++ server/src/utils/SocketClients.js
@@ -2,20 +2,27 @@ const { isEmpty, keys } = require('lodash');
 
 class SocketClients  {
 	constructor() {
-		this._clients = {};
-	}
+	  this._clients = {};
+	  this._lastClient = {};
+  }
 	isOneConnected() {
-		return !isEmpty(this._clients);
-	}
-	setClient(client) {
-		this._clients = {
-			...this._clients,
-			[client.id]: client,
+	  //return !isEmpty(this._clients);
+		return !!this._lastClient
+  }
+  setClient(client) {
+		if(this._lastClient) {
+			delete this._lastClient;
 		}
-	}
-	removeClient(client) {
-		delete this._clients[client.id];
-	}
+	  this._lastClient = client
+    // this._clients = {
+    //   //...this._clients,
+    //   [client.id]: client,
+    // }
+  }
+  removeClient(client) {
+    //delete this._clients[client.id];
+	  delete this._lastClient;
+  }
 }
 
 const socketClients = new SocketClients();
